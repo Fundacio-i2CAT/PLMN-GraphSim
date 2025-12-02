5253cc7240075d1fe21f1d280f99e0291d4ac1ea
Sergio Gimenez <sergio.gimenez@i2cat.net>
2025-12-02 15:16:26 -0500

    Use GUPF accordingly

--- src/Simulation/Metrics/Metrics.jl
+++ src/Simulation/Metrics/Metrics.jl
@@ -11,38 +11,27 @@ include("6GRUPA.jl")
 @resumable function monitor_metrics(env, sim_state::SimGlobalState)
     while true
         current_time = now(env)
-        # 1. Collect Specific Metrics
         metrics_5g = collect_5g_metrics(sim_state)
         metrics_6grupa = collect_6grupa_metrics(sim_state)
-
-        # 2. Update History
         update_history!(sim_state, current_time, metrics_5g, metrics_6grupa)
-
         @yield timeout(env, 1.0) # Sample every 1.0 time unit
     end
 end
 
 function update_history!(sim_state, current_time, metrics_5g, metrics_6grupa)
-    # --- Record History ---
     push!(sim_state.history_time, current_time)
-    
     push!(sim_state.history_total_5g_mb, metrics_5g.total)
     push!(sim_state.history_max_upf_5g_mb, metrics_5g.max)
     push!(sim_state.history_mean_upf_5g_mb, metrics_5g.mean)
     push!(sim_state.history_median_upf_5g_mb, metrics_5g.median)
-
     push!(sim_state.history_total_6grupa_mb, metrics_6grupa.total)
-    push!(sim_state.history_max_upf_6grupa_mb, metrics_6grupa.max)
-    push!(sim_state.history_mean_upf_6grupa_mb, metrics_6grupa.mean)
-    push!(sim_state.history_median_upf_6grupa_mb, metrics_6grupa.median)
-
+    push!(sim_state.history_max_gupf_6grupa_mb, metrics_6grupa.max)
+    push!(sim_state.history_mean_gupf_6grupa_mb, metrics_6grupa.mean)
+    push!(sim_state.history_median_gupf_6grupa_mb, metrics_6grupa.median)
     push!(sim_state.history_mean_entries_6grupa, metrics_6grupa.mean_entries)
     push!(sim_state.history_median_entries_6grupa, metrics_6grupa.median_entries)
-
-    # --- Detailed History ---
     push!(sim_state.history_per_upf_5g_mb, copy(metrics_5g.per_upf_mb))
     push!(sim_state.history_per_upf_entries_5g, copy(metrics_5g.per_upf_entries))
-    
-    push!(sim_state.history_per_upf_6grupa_mb, copy(metrics_6grupa.per_upf_mb))
-    push!(sim_state.history_per_upf_entries_6grupa, copy(metrics_6grupa.per_upf_entries))
+    push!(sim_state.history_per_gupf_6grupa_mb, copy(metrics_6grupa.per_gupf_mb))
+    push!(sim_state.history_per_gupf_entries_6grupa, copy(metrics_6grupa.per_gupf_entries))
 end
--- src/Types.jl
+++ src/Types.jl
@@ -91,9 +91,9 @@ mutable struct SimGlobalState
     history_median_upf_5g_mb::Vector{Float64}
     
     history_total_6grupa_mb::Vector{Float64}
-    history_max_upf_6grupa_mb::Vector{Float64}
-    history_mean_upf_6grupa_mb::Vector{Float64}
-    history_median_upf_6grupa_mb::Vector{Float64}
+    history_max_gupf_6grupa_mb::Vector{Float64}
+    history_mean_gupf_6grupa_mb::Vector{Float64}
+    history_median_gupf_6grupa_mb::Vector{Float64}
 
     history_mean_entries_6grupa::Vector{Float64}
     history_median_entries_6grupa::Vector{Float64}
@@ -101,8 +101,8 @@ mutable struct SimGlobalState
     # Detailed History (Per UPF over time)
     history_per_upf_5g_mb::Vector{Vector{Float64}}
     history_per_upf_entries_5g::Vector{Vector{Int}}
-    history_per_upf_6grupa_mb::Vector{Vector{Float64}}
-    history_per_upf_entries_6grupa::Vector{Vector{Int}}
+    history_per_gupf_6grupa_mb::Vector{Vector{Float64}}
+    history_per_gupf_entries_6grupa::Vector{Vector{Int}}
 end
 
 struct GUPFState6GRUPA
