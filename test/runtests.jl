760876f66943b0871b60b3497d21372264128e5d
Sergio Gimenez <sergio.gimenez@i2cat.net>
2025-12-02 14:45:21 -0500

    Add static cehcks

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,10 @@ using DesJulia6gRupa
     include("AgentGenerationTests.jl")
     include("SimulationTests.jl")
     include("IntegrationTests.jl")
+    
+    if Base.find_package("Aqua") !== nothing && Base.find_package("JET") !== nothing
+        include("qa.jl")
+    else
+        @info "Skipping QA tests (Aqua/JET not found). Run `Pkg.test()` to include them."
+    end
 end
